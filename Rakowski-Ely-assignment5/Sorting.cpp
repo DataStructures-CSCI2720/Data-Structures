2c2ec89eb33151fbca983257ce375aff261c6596
Matt Ely <mwe70005@uga.edu>
2022-04-19 18:54:20 -0400

    updated readme

--- Rakowski-Ely-assignment5/Sorting.cpp
+++ Rakowski-Ely-assignment5/Sorting.cpp
@@ -32,7 +32,7 @@ void Sorting::createList(char argv[]) { // DONE
 void Sorting::createRList() {
     srand(time(0));
     for (int i = 0; i < length; i++) {
-        values[i] = rand() % 10000;
+        values[i] = rand() % length;
     } // for
 } // createRList
 
